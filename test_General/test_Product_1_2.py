2c989d69df41ca24fb8f2613a79233fa95be0c28
Ori Baider <oribaider@gmail.com>
2024-04-03 16:35:40 +0300

    new tests 1_2...

--- test_General/test_Product_1_2.py
+++ test_General/test_Product_1_2.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from Project_Data.Login_func import *
 
 # from Project_Data.Locators_and_DB_Info import *
@@ -66,6 +68,80 @@ def test_1_2_3():
     time.sleep(3)
     first_item = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab))
     first_item.click()
+    price_1st = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_price))
+    price_1st.send_keys(Keys.CONTROL + "a")
+    price_1st.send_keys(Keys.BACKSPACE)
+    price_1st.send_keys("30")
+    time.sleep(3)
+    price_next_btn = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_submit_btn))
+    price_next_btn.click()
+    price_store = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_store))
+    price_store.send_keys("שופרסלטסטטטטטטט")
+    price_store.send_keys(Keys.ENTER)
+    price_next_btn2 = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_submit_btn2))
+    price_next_btn2.click()
+    time.sleep(2)
+    price_next_btn2.click()
+    time.sleep(2)
+    price_next_btn2.click()
+    price_tab = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab_price))
+    price_tab_inner = price_tab.get_attribute("innerText")
+    assert price_tab_inner == "30"
+    time.sleep(5)
+    my_driver.close()
+    my_driver.quit()
+
+
+def test_1_2_4():
+    my_driver = login_web()
+    tab_product = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_tab))
+    tab_product.click()
+    time.sleep(3)
+    first_item = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab))
+    first_item.click()
+    add_name_field = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_name))
+    add_name_field.send_keys(Keys.CONTROL + "a")
+    add_name_field.send_keys(Keys.BACKSPACE)
+    add_name_field.send_keys("חנות מגניבה")
+    time.sleep(3)
+    next_btn = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_submit_btn))
+    next_btn.click()
+    price_store = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_store))
+    price_store.send_keys("שופרסלטסטטטטטטט")
+    price_store.send_keys(Keys.ENTER)
+    next_btn.click()
+    next_btn.click()
+    next_btn.click()
+    product_name = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab_name))
+    product_name_inner = product_name.get_attribute("innerText")
+    assert product_name_inner == "חנות מגניבה"
+    time.sleep(5)
+    my_driver.close()
+    my_driver.quit()
+
+def test_1_2_5():
+    my_driver = login_web()
+    tab_product = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_tab))
+    tab_product.click()
+    time.sleep(3)
+    first_item = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab))
+    first_item.click()
+    add_name_field = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_MKT))
+    add_name_field.send_keys(Keys.CONTROL + "a")
+    add_name_field.send_keys(Keys.BACKSPACE)
+    add_name_field.send_keys("7290000281111")
+    time.sleep(3)
+    next_btn = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_submit_btn))
+    next_btn.click()
+    price_store = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_add_store))
+    price_store.send_keys("שופרסלטסטטטטטטט")
+    price_store.send_keys(Keys.ENTER)
+    next_btn.click()
+    next_btn.click()
+    next_btn.click()
+    product_MKT = WebDriverWait(my_driver, 10).until(EC.presence_of_element_located(products_1st_tab_MKT))
+    product_MKT_inner = product_MKT.get_attribute("innerText")
+    assert product_MKT_inner == "7290000281111"
     time.sleep(5)
     my_driver.close()
     my_driver.quit()
