6da6e6f733b31aa3c44c56fc26a9ad88c511a69d
koren-yeruham <koren381@gmail.com>
2024-04-05 16:30:30 +0300

    tests 4.6

--- Project_Data/Locators_and_DB_Info.py
+++ Project_Data/Locators_and_DB_Info.py
@@ -56,31 +56,37 @@ btn_arrow_sign = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[2
 btn2_arrow_sign = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/span[6]/i')
 
 "eTrado Locators"
+#לכל טסט etrado_tab
 etrado_tab = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/nav/div[2]/a[17]/span[2]')
 btn_more_option_menu_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[1]/div/span/i')
 btn_add = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[1]/div[2]/div/div[1]')
-btn_sum = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div/span/div/input')
+field_sum = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div/span/div/input')
+#כפתורים בתהליך הוספה לטסט 4.6.1
 btn_next_sum = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[5]/input')
 btn_next_details = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[5]/input[2]')
 btn_next_results = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[5]/input[2]')
 btn_add_etrado_payment = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
+#
 text_creation_date_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[2]/div[1]/table/tbody/tr[17]/td[4]/div')
+#כפתורים
 btn_update_sum = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
 btn_next_detail_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[5]/input[2]')
 btn_next_result_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
 btn_update_etrado_payment = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
 btnn_next_etrado_sum = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[5]/input')
 sum_element_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[2]/div[1]/table/tbody/tr[17]/td[1]')
-btn_ID_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[1]/span/div/input')
-btn_updat_id_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
-btn_updat_id_etrado_payment = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
-BTN_UPDATE_ID_ETRADO_RESULT = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/input')
-success_msg_update = (By.XPATH, '//*[@id="af8c8y1v5p"]/div[1]')
 
+#test-4.6.4
+field_ID_etrado = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[1]/span/div/input')
 ###test-4.6.5
 Payment_period_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[2]/span/div/input')
-
-
+Account_Number_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[3]/span/div/input')
+bank_name_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[4]/div[1]/span[1]/div[1]')
+bank_leumi_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[4]/div[1]/span[1]/div[2]/div/div')
+bank_name_input = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[4]/div[1]/span[1]/div/input')
+Bank_code_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[5]/span/div/input')
+Branch_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[6]/span/div/input')
+Remarks_element = (By.XPATH, '//*[@id="root"]/div[1]/div[4]/div/div/div/form/div[3]/div[7]/span/div/input')
 
 
 "stores Locators"
